a9d23d4a1e532f816340e7e085c9d0e1d38d2bc6
Kelvin Hsu <skps0102@gmail.com>
2022-01-15 17:14:49 +0800

    rename login to index

--- js/products.js
+++ js/products.js
@@ -1,45 +1,46 @@
 import { createApp } from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.26/vue.esm-browser.min.js";
 
-const url = 'https://vue3-course-api.hexschool.io/v2'; // 請加入站點
-const path = 'skps0102'; // 請加入個人 API Path
-
 const app = createApp({
     data() {
         return {
+            url: 'https://vue3-course-api.hexschool.io/v2',
+            api_Path: 'skps0102',
             products: [],
             itemTemp: {},
         }
     },
     methods: {
         checkLogin() {
-            // 取得 Token
-            const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-            console.log('get Cookie token', token);
-            axios.defaults.headers.common['Authorization'] = token;
-
             // 確認是否登入
-            axios.post(`${url}/api/user/check`)
-                .then((resp) => {
-                    console.log('resp ', resp);
+            axios.post(`${this.url}/api/user/check`)
+                .then((res) => {
+                    console.log('resp ', res);
                     this.getProductsData();
                 })
-                .catch((error) => {
+                .catch((err) => {
+                    alert(err.data.message);
                     //coolkie不存在會導回登入頁面
-                    window.location = 'login.html';
+                    window.location = 'index.html';
                 })
         },
         getProductsData() {
-            axios.get(`${url}/api/${path}/admin/products`)
-                .then((resp) => {
-                    const { products } = resp.data;
+            axios.get(`${this.url}/api/${this.api_Path}/admin/products`)
+                .then((res) => {
+                    const { products } = res.data;
                     this.products = products;
                 })
-                .catch((error) => {
-                    console.log(error);
+                .catch((err) => {
+                    alert(err.data.message);
+                    console.log(err);
                 })
         }
     },
     created() {
+        // 取得 Token
+        const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+        console.log('get Cookie token', token);
+        axios.defaults.headers.common['Authorization'] = token;
+
         this.checkLogin();
     }
 });
